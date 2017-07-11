b3891fd3d5365c5ba87136376159e8bd335415b1
ysera34 <ycyoon34@gmail.com>
2017-07-11 14:55:16 +0900

    chapter01

--- chapter01/mvc.js
+++ chapter01/mvc.js
@@ -17,10 +17,30 @@ var assertEqual = function(value1, value2, message) {
     }
 };
 
+
+// 익명 함수로 범위를 캡슐화해서 불필요한 전역 변수 생성을 막았다.
 var Person = function(name) {
     this.name = name;
 }
 
+// 생성자 함수의 이름은 카멜케이스로 표시해 다른 일반 함수와 구분하는 것이 관례다.
 var bellinger = new Person("Bellinger");
 
 assert(bellinger instanceof Person);
+//Person("aaron"); // => undefinded
+
+var Class = function() {
+    var klass = function() {
+        this.init.apply(this, arguments);
+    };
+    klass.prototype.init = function(){};
+    return klass;
+};
+
+var Person = new Class;
+
+Person.prototype.init = function(){
+    
+}
+
+var person = new Person;
